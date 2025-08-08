1cf0fd3f0c9582c9b95c0babc6c9f760a14279a3
Mina Mikhail <minamikhail504@gmail.com>
2025-08-07 22:46:12 -0400

    nav fix

--- my-app/src/Components/nav/nav.jsx
+++ my-app/src/Components/nav/nav.jsx
@@ -53,7 +53,7 @@ const Nav = () => {
     useEffect(() => {
         window.addEventListener('resize', function() {
             if (window.innerWidth >= 1000) {
-                isDropdownOpen = true;
+                isDropdownOpen.current = true;
                 toggleDropdown();
             }
         })
